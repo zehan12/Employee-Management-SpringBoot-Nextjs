9e0b4a8ead3f2dfc3094f026c52e1576798d83ab
Zehan Khan <zkhan@hootboard.com>
2024-03-24 05:44:36 +0530

    feat: :lock: add dotenv-java with  .env var file and added dataSource to connect db

--- server/src/main/java/com/backend/server/ServerApplication.java
+++ server/src/main/java/com/backend/server/ServerApplication.java
@@ -2,6 +2,11 @@ package com.backend.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import javax.sql.DataSource;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -10,4 +15,17 @@ public class ServerApplication {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+	@Bean
+	@Primary
+	public DataSource dataSource() {
+		Dotenv dotenv = Dotenv.configure().load();
+		String url = dotenv.get("SPRING_DATASOURCE_URL");
+
+		// Use DriverManagerDataSource to create a data source
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
+		dataSource.setUrl(url);
+
+		return dataSource;
+	}
 }
